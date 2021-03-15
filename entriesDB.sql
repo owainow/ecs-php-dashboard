f6670526edf248b70b2c748bab072bbb32f2bdc0
owainow <owain.osborne@tigerbay.uk.com>
2021-03-15 16:44:14 +0000

    removed '

--- entriesDB.sql
+++ entriesDB.sql
@@ -2,7 +2,7 @@ CREATE TABLE Entries (
   Rider_ID varchar (100),
   Rider varchar(150),
   Horse varchar (100),
-  'Number' varchar (150),
+  Number varchar (150),
   Email varchar(100),
   Address varchar (100),
   Postcode varchar(7),
